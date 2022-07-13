d39f99f3ac36131a7f7ee6dc351148fa9a4db1ac
cgustafsonx <cgustafsonx@gmail.com>
2022-07-13 11:03:56 +0200

    Added JS clock timer

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const days = document.getElementById("days");
+console.log(days);
 const hours = document.getElementById("hours");
+console.log(hours);
 const minutes = document.getElementById("minutes");
+console.log(minutes);
 const seconds = document.getElementById("seconds");
+console.log(seconds);
 function goCountdown() {
-  console.log("Hi!");
-  //the final date
-  const fest = new Date(2023, 10, 13);
+  const fest = new Date(2022, 10, 13);
+  console.log(fest);
 
-  // the system date
   const current = new Date();
+  console.log(current);
 
-  //time remaining
   const sec = (fest - current) / 1000;
+  console.log(sec);
   const d = Math.floor(sec / 3600 / 24);
+  console.log(d);
   const h = Math.floor(sec / 3600) % 24;
+  console.log(h);
   const m = Math.floor(sec / 60) % 60;
+  console.log(m);
   const s = Math.floor(sec) % 60;
+  console.log(s);
   days.innerText = d;
   hours.innerText = h;
   minutes.innerText = m;
@@ -23,5 +30,3 @@ function goCountdown() {
 }
 goCountdown();
 setInterval(goCountdown, 1000);
-
-// console.log("Hello World");
